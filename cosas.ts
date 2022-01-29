f438571e83f602f9b64361b8d96b4108230c9f7f
collo9823 <collo9823@gmail.com>
2022-01-28 22:51:47 -0300

    primer commit

---
+++ cosas.ts
@@ -0,0 +1,62 @@
+// const getAll = function(a:number, b:number){
+//     return a + b;
+
+// };
+
+
+// export{getAll};
+
+
+
+let supermarketList = [
+    {
+        producto: "carne",
+        precio: 700,
+    },
+    {
+        producto: "cebolla",
+        precio: 100,
+    },
+    {
+        producto: "morron",
+        precio: 250,
+    },
+    {
+        producto: "puré",
+        precio: 70,
+    },
+    {
+        producto: "papa",
+        precio: 100,
+    },
+    {
+        producto: "zanahoria",
+        precio: 100,
+    },
+    {
+        producto: "pepsi",
+        precio: 150,
+    },
+    {
+        producto: "monster",
+        precio: 200,
+    },
+    {
+        producto: "te",
+        precio: 130,
+    },
+    {
+        producto: "pan",
+        precio: 130,
+    },
+        
+    ]
+
+
+    function getAll(){
+
+    };
+
+    function getById(id:number){
+
+    };
---
+++ index.ts
@@ -0,0 +1,8 @@
+// import { getAll } from "./cosas";
+
+// function main(){
+//     console.log(getAll(4, 5));
+// }
+
+// main();
+
